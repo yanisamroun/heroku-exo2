05a3c4fd0b4fbecf7ac790b8ef0a01d291f5fcae
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-10 15:12:36 +0200

    Anonymous function for Emoji model

--- models/Emoji.js
+++ models/Emoji.js
@@ -1,14 +1,16 @@
 // DataTypes est à importer dans chaque fichier de modèle
 const {DataTypes} = require('sequelize');
 
-module.exports = (sequelize) => sequelize.define('Emoji', {
-    icon: {
-        type: DataTypes.STRING,
-        allowNull: false 
-    }
-},
- {
-     tableName: 'emoji',
-     underscored: true,
-     // timestamps: false  -> si on veut enlever le created_at ...
- })
+module.exports = function (sequelize) {
+    return sequelize.define('Emoji', {
+        icon: {
+            type: DataTypes.STRING,
+            allowNull: false 
+        }
+    },
+     {
+         tableName: 'emoji',
+         underscored: true,
+         // timestamps: false  -> si on veut enlever le created_at ...
+     })
+}
